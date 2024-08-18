a4afa0abac868de3317e6cb8020a25c114c0497a
rael017 <fraphael011@gmail.com>
2024-08-18 11:44:23 -0300

    freemwork php

---
+++ Includes/Exemple-App.php
@@ -0,0 +1,41 @@
+<?php
+
+use \App\Utils\Envs;
+use \App\Utils\MainView;
+use \App\Http\Middlewares\Config;
+require( __DIR__.'/../vendor/autoload.php');
+
+
+
+date_default_timezone_set('America/Sao_Paulo');
+
+Envs::load(__DIR__.'/../');
+
+define('URL',getenv('URL'));
+define('BASE',getenv('BASE'));
+define('DIR',getenv('DIR'));
+
+define('DB_HOST',getenv('DB_HOST'));
+define('DB_NAME',getenv('DB_NAME'));
+define('DB_USER',getenv('DB_USER'));
+define('DB_PASS',getenv('DB_PASS'));
+
+MainView::init([
+	"URL" =>URL,
+	"BASE"=>BASE,
+	"DIR" =>DIR
+]);
+
+Config::setMap([
+	'Manutention'=>\App\Http\Middlewares\Manutention::class,
+	'requiered-admin-logout'=>\App\Http\Middlewares\RequireAdminLogout::class,
+	'requiered-admin-login'=>\App\Http\Middlewares\RequireAdminLogin::class,
+	'api'=>\App\Http\Middlewares\Api::class
+	
+]);
+
+Config::setDefault([
+	'Manutention'
+]);
+?>
+
